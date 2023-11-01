e581cbb81e0ce0e45d5dcbc2b2098e8528600fa1
Trịnh Quyền Diệp <Trinhdiep.zoro@gmail.com>
2023-11-01 23:55:02 +0700

    Update ESP8266_controlleLed.ino
    update_1/11/2023

--- ESP8266_controlleLed/ESP8266_controlleLed.ino
+++ ESP8266_controlleLed/ESP8266_controlleLed.ino
@@ -14,6 +14,9 @@
 #include <FirebaseESP8266.h>
 #include <ArduinoJson.h>
 #include <TimeLib.h>
+#include <Wire.h>
+#include <time.h>
+#include <vector>
 char ssid[] = "PeiD";
 char pass[] = "12345678";
 #define WIFI_SSID "PeiD"
@@ -23,31 +26,69 @@ char pass[] = "12345678";
 #define Button D7
 #define Led2 D2
 #define Button2 D3
-bool previousButtonState = HIGH; 
+bool previousButtonState = LOW; 
 bool previousButtonState2 = HIGH; // Trạng thái trước đó của nút bấm
-
+int timezone = 7*3600;
+int dat = 0;
+int solanbat = 0;
+int solantat = 0;
 FirebaseData firebaseData;
 String path = "/";
 FirebaseJson json;
 FirebaseAuth auth;
 FirebaseConfig config;
-String getCurrentDateTime() {
+String getCurrentDate() {
   // Lấy thời gian hiện tại
-  time_t currentTime = now();
-  
-  // Lấy thông tin ngày giờ từ thời gian hiện tại
-  int currentYear = year(currentTime);
-  int currentmonth = month(currentTime);
-  int currentday = day(currentTime);
-  int currenthour = hour(currentTime);
-  int currentminute = minute(currentTime);
-  int currentsecond = second(currentTime);
-  
-  // Tạo chuỗi biểu diễn ngày giờ
-  String dateTime = String(currentYear) + "-" + String(currentmonth) + "-" + String(currentday) + " " + String(currenthour) + ":" + String(currentminute) + ":" + String(currentsecond);
+  time_t now = time(nullptr);
+  struct tm* p_tm = localtime(&now);
+  String ngay  = String(p_tm -> tm_mday);
+  String thang = String(p_tm -> tm_mon + 1) ;
+  String nam = String(p_tm -> tm_year + 1900);
   
   // Trả về chuỗi ngày giờ
-  return dateTime;
+  String date = ngay + "-" + thang +"-" + nam ;
+  return date;
+}
+String getCurrentTime() {
+  // Lấy thời gian hiện tại
+  time_t now = time(nullptr);
+  struct tm* p_tm = localtime(&now);
+  String gio = String(p_tm -> tm_hour);
+  String phut = String(p_tm -> tm_min);
+  String giay = String(p_tm -> tm_sec);
+  // Trả về chuỗi ngày giờ
+  String Time = gio + ":" + phut + ":" + giay;
+  return Time;
+}
+std::vector<String> getAllDataFromFirebase(dataPath) {
+  String data[] = Firebase.getString(firebaseData, dataPath);
+  // String dataValues = data.substring(1, data.length() - 1); // Loại bỏ dấu ngoặc đơn ở đầu và cuối
+  // int itemCount = 0;
+
+  // // Đếm số phần tử trong danh sách
+  // for (int i = 0; i < dataValues.length(); i++) {
+  //   if (dataValues[i] == ',') {
+  //     itemCount++;
+  //   }
+  // }
+  // itemCount++;  // Tăng thêm một cho phần tử cuối cùng
+
+  // String dataArray[itemCount];
+  // int startIndex = 0;
+  // int endIndex = dataValues.indexOf(',');
+
+  // for (int i = 0; i < itemCount; i++) {
+  //   if (endIndex == -1) {
+  //     dataArray[i] = dataValues.substring(startIndex);
+  //   } else {
+  //     dataArray[i] = dataValues.substring(startIndex, endIndex);
+  //   }
+
+  //   startIndex = endIndex + 2; // Bỏ qua dấu phẩy và dấu cách
+  //   endIndex = dataValues.indexOf(',', startIndex);
+  // }
+
+  return dataArray;
 }
 void setup() {
   Serial.begin(9600);
@@ -67,10 +108,24 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.println("IP address: " + WiFi.localIP().toString());
 
+  configTime(timezone,dat,"pool.ntp.org","time.nist.gov");
+  Serial.println("connecting...");
+  while(!time(nullptr)){
+    Serial.println(".");
+    delay(500);
+  }
+  Serial.println("time response ok...");
+
   config.database_url = "https://iot-nhom-7-d70d7-default-rtdb.firebaseio.com/";
   config.signer.tokens.legacy_token = "QNZWTt0SoQivgDeFSL5imCXQxGr3gwMEl9Cq49O2";
   
   Firebase.begin(&config, &auth);
+  String currentDate = getCurrentDate();
+  String path = path + "/" + BLYNK_AUTH_TOKEN + "/" + "Led" +"/" + currentDate );
+  String data[] = getAllDataFromFirebase(path);
+  for(int i = 0 ; i < length(data); i ++){
+    Serial.println(data[i]);
+  }
 }
 
 BLYNK_WRITE(V2) {
@@ -99,9 +154,10 @@ void buttonPressed(int buttonPin, int ledPin, bool &state) {
     bool finalButtonState = digitalRead(buttonPin);
 
     if (finalButtonState == LOW && finalButtonState != state) {
-      String currentTime = getCurrentDateTime(); // Thời gian hiện tại
-      String data = currentTime + "_led1_" + (state ? "ON" : "OFF");
-      Firebase.setString(firebaseData, path + "/data", data);
+      String currentDate = getCurrentDate(); // Thời gian hiện tại
+      String currentTime = getCurrentTime();
+      Firebase.setString(firebaseData, path + "/" + BLYNK_AUTH_TOKEN + "/" + "Led" +"/" + currentDate + "/" + (digitalRead(ledPin) ? "OFF" : "ON") + "/" + (digitalRead(ledPin) ? String(solantat++) : String(solanbat++)) , currentTime);
+      Serial.println((digitalRead(ledPin) ? "OFF" : "ON"));
       digitalWrite(ledPin, !digitalRead(ledPin));
       int ledState = digitalRead(ledPin);
 
@@ -149,6 +205,4 @@ void buttonPressed(int buttonPin, int ledPin, bool &state) {
 void loop() {
   Blynk.run();
   buttonPressed(Button, Led, previousButtonState);
-  buttonPressed(Button2, Led2, previousButtonState2);
-  Firebase.setInt(firebaseData, path+"/led1", random(0,100));//chay thu ket noi firebase
 }
