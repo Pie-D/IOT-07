f4b13989fac6f7d53421c3eedfef2693b755bf18
hungwnguyen <hanoihung123321@gmail.com>
2023-11-13 12:52:08 +0700

    update chức năng cho người dùng kết nối wifi cho esp8266 mà không cần khai báo tên đăng nhập + mật khẩu

--- ESP8266_controlleLed/ESP8266_controlleLed.ino
+++ ESP8266_controlleLed/ESP8266_controlleLed.ino
@@ -1,5 +1,3 @@
-
-
 #define BLYNK_PRINT Serial
 
 /* Fill in information from Blynk Device Info here */
@@ -7,7 +5,9 @@
 #define BLYNK_TEMPLATE_NAME "TEST"
 #define BLYNK_AUTH_TOKEN "wo-F6FyU_cajnKQ6-B6BWDfiPRHeWbBj"
 
-
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
 #include <Blynk.h>
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
@@ -17,17 +17,14 @@
 #include <Wire.h>
 #include <time.h>
 #include <vector>
-char ssid[] = "PeiD";
-char pass[] = "12345678";
-#define WIFI_SSID "PeiD"
-#define WIFI_PASSWORD "12345678"
 
 #define Led D6
-#define Button D7
 #define Led2 D2
+#define Button D7
 #define Button2 D3
-bool previousButtonState = LOW; 
-bool previousButtonState2 = HIGH; // Trạng thái trước đó của nút bấm
+#define ButtonReset D4
+
+bool previousButtonState = HIGH, previousButtonState2 = HIGH, previousButtonState3 = HIGH; 
 int timezone = 7*3600;
 int dat = 0;
 FirebaseData firebaseData;
@@ -36,6 +33,8 @@ String dataPath = "wo-F6FyU_cajnKQ6-B6BWDfiPRHeWbBj/Led/1-11-2023";
 FirebaseJson json;
 FirebaseAuth auth;
 FirebaseConfig config;
+WiFiManager wifiManager;
+
 String getCurrentDate() {
   // Lấy thời gian hiện tại
   time_t now = time(nullptr);
@@ -122,24 +121,32 @@ int deleteDataFromFirebase(String datapath) {
   }
 
 }
-void setup() {
+
+void SetUpWifi(){
+  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
   Serial.begin(9600);
-  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  wifiManager.setCustomHeadElement("<style>html{filter: invert(100%); -webkit-filter: invert(100%);}</style>");
+  if (!wifiManager.autoConnect("NHÀ THÔNG MINH"))
+  {
+    Serial.println("KẾT NỐI THẤT BẠI ĐANG RESET ESP CHỜ.....");
+    ESP.reset();//Nếu kết nối thất bại, thử kết nối lại bằng cách reset thiết bị
+    delay(800);
+  }
+  else{
+    SetupBlynk();
+    SetupFirebase();
+  }
+}
+
+void SetupBlynk(){
+  String ssid = wifiManager.getWiFiSSID();
+  String password = wifiManager.getWiFiPass();
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid.c_str(), password.c_str());
+  Serial.println("Connected to WiFi:");
   pinMode(Led, OUTPUT);
   pinMode(Button, INPUT_PULLUP);
   pinMode(Led2, OUTPUT);
   pinMode(Button2, INPUT_PULLUP);  // Thiết lập chân nút bấm với chế độ INPUT_PULLUP
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println();
-  Serial.println("WiFi connected");
-  Serial.println("IP address: " + WiFi.localIP().toString());
-
   configTime(timezone,dat,"pool.ntp.org","time.nist.gov");
   Serial.println("connecting...");
   while(!time(nullptr)){
@@ -147,30 +154,31 @@ void setup() {
     delay(500);
   }
   Serial.println("time response ok...");
-
+}
+void SetupFirebase(){
   config.database_url = "https://iot-nhom-7-d70d7-default-rtdb.firebaseio.com/";
   config.signer.tokens.legacy_token = "QNZWTt0SoQivgDeFSL5imCXQxGr3gwMEl9Cq49O2";
-  
   Firebase.begin(&config, &auth);
   getAllDataFromFirebase(dataPath);
-  deleteDataFromFirebase(dataPath);
+  //deleteDataFromFirebase(dataPath);
 }
 
-BLYNK_WRITE(V2) {
-  int button = param.asInt();
-  if (button == 1) {
-    digitalWrite(Led, HIGH);
+void setup() {
+  SetUpWifi();
+}
+
+void Blynk_write(int led, int status){
+  if (status == 1) {
+    digitalWrite(led, HIGH);
   } else {
-    digitalWrite(Led, LOW);
+    digitalWrite(led, LOW);
   }
 }
+BLYNK_WRITE(V2) {
+  Blynk_write(Led, param.asInt());
+}
 BLYNK_WRITE(V7) {
-  int button = param.asInt();
-  if (button == 1) {
-    digitalWrite(Led2, HIGH);
-  } else {
-    digitalWrite(Led2, LOW);
-  }
+  Blynk_write(Led2, param.asInt());
 }
 void buttonPressed(int buttonPin, int ledPin, bool &state) {
   bool currentButtonState = digitalRead(buttonPin);
@@ -215,7 +223,6 @@ void buttonPressed(int buttonPin, int ledPin, bool &state) {
         }
       }
 
-
       String x = String(BLYNK_AUTH_TOKEN) + "/Led/" + currentDate ;
       String soLanBat = getSoLanBat(x + "/ON");
       String soLanTat = getSoLanTat(x + "/OFF");
@@ -232,145 +239,26 @@ void buttonPressed(int buttonPin, int ledPin, bool &state) {
         
       }
     } 
-
     state = finalButtonState;
   }
 }
-// void buttonPressed(int buttonPin, int ledPin, bool &state) {
-//   bool currentButtonState = digitalRead(buttonPin);
 
-//   if (currentButtonState != state) {
-//     delay(50);
-
-//     bool finalButtonState = digitalRead(buttonPin);
-
-//     if (finalButtonState == LOW && finalButtonState != state) {
-//       String currentDate = getCurrentDate(); // Thời gian hiện tại
-//       String currentTime = getCurrentTime();
-//       int firstDashIndex = currentDate.indexOf('-');
-//       int secondDashIndex = currentDate.indexOf('-', currentDate.indexOf('-') + 1);
-//       String dayStr = "";  // Tìm vị trí của dấu gạch đầu tiên
-//       String monthStr = "";
-//       String yearStr = "";
-//       int dayInt;
-//       int monthInt;
-//       int yearInt;
-//       dayStr = currentDate.substring(0, firstDashIndex);
-//       monthStr = currentDate.substring(firstDashIndex + 1, secondDashIndex);
-//       yearStr = currentDate.substring(secondDashIndex + 1);  // Lấy chuỗi con từ sau dấu gạch thứ hai đến hết chuỗi
-//       dayInt = dayStr.toInt();
-//       monthInt = monthStr.toInt();
-//       yearInt = yearStr.toInt();
-
-//       if(dayInt == 2){
-//         if(monthInt != 1){
-//           monthInt = monthInt - 1;
-//         } else {
-//           monthInt = 12;
-//           yearInt = yearInt - 1;
-//         }
-//         int count = 0;
-//         for(int i = 31 ; i > 0 ; i --){
-//           String x = String(BLYNK_AUTH_TOKEN) + "/Led/" + String(i) + "-" + String(monthInt) + "-" + String(yearInt);
-//           if(deleteDataFromFirebase(x) % 2 == 0){
-//             count ++;
-//           }
-//           if(count == 4 ) break;
-//         }
-//       }
-
-
-//       String x = String(BLYNK_AUTH_TOKEN) + "/Led/" + currentDate ;
-//       String soLanBat = getSoLanBat(x + "/ON");
-//       String soLanTat = getSoLanTat(x + "/OFF");
-//       Serial.println(x); 
-
-//       unsigned long lastUpdateTime = 0;
-//       unsigned long updateInterval = 5000; // 5 giây
-//       unsigned long timeLive = 0;
-//       unsigned long ledx = digitalRead(ledPin);
-//       if (!ledx) {
-//         Firebase.setInt(firebaseData, path + BLYNK_AUTH_TOKEN + "/" + "Led" +"/" + currentDate + "/" + "ON"+ "/" +soLanBat + "/"  + currentTime,0);
-//         bool ButtonState = 0;
-//         // Serial.print("digitalRead(ledPin) : ");
-//         // Serial.println(digitalRead(ledPin));
-//         Serial.println((digitalRead(ledPin) ? "OFF" : "ON"));
-//         digitalWrite(ledPin, !digitalRead(ledPin));
-//         while (ledx == ButtonState) {
-//           if (millis() - lastUpdateTime >= updateInterval) {
-//             // Thực hiện cập nhật đèn mỗi 5 giây
-//             lastUpdateTime = millis();
-//             timeLive += 5;
-//             Firebase.setInt(firebaseData, path + BLYNK_AUTH_TOKEN + "/" + "Led" +"/" + currentDate + "/" + "ON"+ "/" +soLanBat + "/"  + currentTime,timeLive);
-//           }
-//         // Serial.print("digitalRead(ledPin) : ");
-//         // Serial.println(digitalRead(ledPin));
-//         ButtonState = digitalRead(buttonPin); 
-//         // Serial.print("digitalRead(buttonPin) : ");
-//         // Serial.println(ButtonState);
-//         ledx = 1;
-          
-//         }
-//         int ledState = digitalRead(ledPin);
-
-//         if (ledPin == Led) {
-//           Blynk.virtualWrite(V2, ledState);
-//         } else if (ledPin == Led2) {
-//           Blynk.virtualWrite(V7, ledState);
-          
-//         }
-//       } else {
-//         Firebase.setString(firebaseData, path + BLYNK_AUTH_TOKEN + "/" + "Led" + "/" + currentDate + "/" + "OFF" + "/" + soLanTat, currentTime);
-
-//         Serial.println((digitalRead(ledPin) ? "OFF" : "ON"));
-//         digitalWrite(ledPin, !digitalRead(ledPin));
-//         int ledState = digitalRead(ledPin);
-
-//         if (ledPin == Led) {
-//           Blynk.virtualWrite(V2, ledState);
-//         } else if (ledPin == Led2) {
-//           Blynk.virtualWrite(V7, ledState);
-          
-//         }
-//       }
-      
-//     } 
-
-//     state = finalButtonState;
-//   }
-// }
-
-
-// void buttonPressed(int buttonPin, int ledPin, bool &state) {
-//   bool currentButtonState = digitalRead(buttonPin);
-
-//   if (currentButtonState != state) {
-//     delay(50);
-
-//     bool finalButtonState = digitalRead(buttonPin);
-
-//     if (finalButtonState == LOW && finalButtonState != state) {
-//       digitalWrite(ledPin, !digitalRead(ledPin));
-//       int ledState = digitalRead(ledPin);
-
-//       // Gửi dữ liệu lên Firebase Realtime Database
-//       String ledName = (ledPin == Led) ? "led1" : "led2"; // Tên đèn tương ứng
-//       // String currentTime = String(millis()); // Thời gian hiện tại
-//       // String data = currentTime + "_" + ledName + "_" + (ledState ? "ON" : "OFF");
-//       // Firebase.setString(firebaseData, path + "/data", data);
-
-//       if (ledPin == Led) {
-//         Blynk.virtualWrite(V2, ledState);
-//       } else if (ledPin == Led2) {
-//         Blynk.virtualWrite(V7, ledState);
-//       }
-//     }
-
-//     state = finalButtonState;
-//   }
-// }
+void buttonPressed(int buttonPin, bool &state) {
+  bool currentButtonState = digitalRead(buttonPin);
+  if (currentButtonState != state) {
+    delay(50);
+    bool finalButtonState = digitalRead(buttonPin);
+    if (finalButtonState == LOW && finalButtonState != state) {
+      wifiManager.resetSettings();        // xóa bộ nhớ wifi cũ AUTO QUAY VỀ Void setup do đó ko chạy lệnh dưới 
+      SetUpWifi();
+    } 
+    state = finalButtonState;
+  }
+}
 
 void loop() {
   Blynk.run();
   buttonPressed(Button, Led, previousButtonState);
+  buttonPressed(Button2, Led2, previousButtonState2);
+  buttonPressed(ButtonReset, previousButtonState3);
 }
